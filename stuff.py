b647646c601fc05ad416734e6a4b180d3d93ba91
Alex Lloyd <113545460+ahlloyd@users.noreply.github.com>
2023-06-07 11:19:21 -0400

    Add files via upload

---
+++ stuff.py
@@ -0,0 +1,25 @@
+from BitString import BitArray
+
+def reverseBits(myBits):
+	tmp1 = myBits
+	tmp2 = 
+	for bit in myBits:
+		tmp.prepend(bit)
+		mostSignificant = tmp[]
+		leastSignificant = tmp[0]
+		for bit in tmp1:
+
+
+    return myBits
+
+myBits = BitArray('0b01101110')
+# 0b01110110 # get here
+# 0b11101100
+# 0b11011001
+# 0b10110011
+# 0b01100111
+# 0b11001110
+# 0b10011101
+# 0b00111011
+# 0b01110110
+reverseBits(myBits)
